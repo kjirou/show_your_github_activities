83e13811b9ce863ed56622008e3e42fd51dae218
kjirou <kjirou.web@gmail.com>
2012-11-28 14:53:05 +0900

    Mod something

--- w/app.js
+++ w/app.js
@@ -15,6 +15,10 @@ $f.parseUrlToParameters = function(url){
     return params;
 }
 
+$a.LAST_CACHED_AT_KEY = 'last_cached_at';
+$a.GITHUB_USER_EVENTS_DATA_KEY = 'github_user_events_data';
+$a.CACHE_LIFETIME = 60 * 10 * 1000; // 10 mins
+$a.GITHUB_API_URL = 'https://api.github.com';
 $a.urlParams = $f.parseUrlToParameters(document.URL);
 $a.githubUsername = $a.urlParams.u;
 $a.frameWidth = ~~$a.urlParams.w;
@@ -31,38 +35,76 @@ $a.adjustStyles = function(size){
         border: $a.frameBorderWidth + 'px solid #DDD'
     });
 }
-$a.githubUserData = null;
+$a.getGithubUserEventsApiUrl = function(username){
+    return $a.GITHUB_API_URL + '/users/' + username + '/events';
+}
 $a.githubUserEventsData = null;
-$a.githubApiUrl = 'https://api.github.com';
-$a.githubUserApiUrl = $a.githubApiUrl + '/users/' + $a.githubUsername;
-$a.githubUserEventsApiUrl = $a.githubUserApiUrl + '/events';
+$a.isEnabledCache = function(){
+    var lastCachedAt;
+    var userEventsData;
+    if (window.localStorage === undefined) {
+        return false;
+    } else {
+        lastCachedAt = parseInt(localStorage.getItem($a.LAST_CACHED_AT_KEY));
+        userEventsData = $.evalJSON(localStorage.getItem($a.GITHUB_USER_EVENTS_DATA_KEY));
+        if (
+            isNaN(lastCachedAt) ||
+            userEventsData === null ||
+            lastCachedAt + $a.CACHE_LIFETIME < (new Date()).getTime()
+        ) {
+            return false;
+        }
+    }
+    return true;
+}
+$a.loadData = function(){
+    var d = $.Deferred();
+    if ($a.isEnabledCache()) {
+        $a.githubUserEventsData = $.evalJSON(localStorage.getItem($a.GITHUB_USER_EVENTS_DATA_KEY));
+        setTimeout(function(){ d.resolve(); }, 1);
+    } else {
+        localStorage.setItem($a.LAST_CACHED_AT_KEY, (new Date()).getTime());
+        $.ajax(
+            $a.getGithubUserEventsApiUrl($a.githubUsername),
+            { dataType:'jsonp', callback:'callback' }
+        ).then(function(data){
+            $a.githubUserEventsData = data;
+            localStorage.setItem($a.GITHUB_USER_EVENTS_DATA_KEY, $.toJSON(data));
+            d.resolve();
+        });
+    }
+    return d;
+}
+$a.getAvatarUrl = function(){
+    if (
+        $a.githubUserEventsData === null ||
+        ~~$a.githubUserEventsData.data.length === 0
+    ) return null;
+    return $a.githubUserEventsData.data[0].actor.avatar_url;
+}
+$a.isSuccessedGettingData = function(){
+    return !!$a.getAvatarUrl();
+}
 
 $a.init = function(){
     $a.adjustStyles([$a.frameWidth, $a.frameHeight]);
 
     $.Deferred().resolve().then(function(){
-        var d1 = $.Deferred();
-        $.ajax($a.githubUserApiUrl, { dataType:'jsonp', callback:'callback' }).then(function(data){
-            $a.githubUserData = data;
-            d1.resolve();
-        });
-        var d2 = $.Deferred();
-        $.ajax($a.githubUserEventsApiUrl, { dataType:'jsonp', callback:'callback' }).then(function(data){
-            $a.githubUserEventsData = data;
-            d2.resolve();
-        });
-        return $.when(d1.promise(), d2.promise()).done(function(){
+        var d = $a.loadData();
+        return $.when(d.promise()).done(function(){
             /* no process */
         }).fail(function(){
             throw new Error('Failed request to github api');
         });
     }).then(function(){
+        if ($a.isSuccessedGettingData() === false) return;
+
         $('#header a').attr({
             href: 'https://github.com/' + $a.githubUsername
         }).text($a.githubUsername);
-        $('#header img').attr('src', $a.githubUserData.data.avatar_url);
+
+        $('#header img').attr('src', $a.getAvatarUrl());
     }).then(function(){
-        console.log($a.githubUserData);
         console.log($a.githubUserEventsData);
     });
 }
